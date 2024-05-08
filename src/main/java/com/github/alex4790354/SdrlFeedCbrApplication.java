5febb78f6601170542630561ae23562b464ddd89
Alex4790354 <9720560@gmail.com>
2024-05-08 18:36:00 +0300

    SDRL initial commit from start.spring.io

---
+++ src/main/java/com/github/alex4790354/SdrlFeedCbrApplication.java
@@ -0,0 +1,13 @@
+package com.github.alex4790354;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdrlFeedCbrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdrlFeedCbrApplication.class, args);
+	}
+
+}
