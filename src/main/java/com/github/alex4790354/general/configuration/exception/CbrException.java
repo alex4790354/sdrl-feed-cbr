6922653efaafbffb76fd658c28e5e6795f6666f2
Alex4790354 <9720560@gmail.com>
2024-06-14 22:26:37 +0300

    Delete for review.

--- src/main/java/com/github/alex4790354/general/configuration/exception/CbrException.java
+++
@@ -1,8 +0,0 @@
-package com.github.alex4790354.general.configuration.exception;
-
-public class CbrException extends RuntimeException {
-
-    public CbrException(String message) {
-        super(message);
-    }
-}
